5ac318d90191e7a233360c2f17606cb305dea710
SkillSwapp Team <skillswapp@example.com>
2026-01-23 03:16:30 +0100

    Use Expo native tsconfigPaths instead of babel-plugin-module-resolver

--- FRONTEND/babel.config.js
+++ FRONTEND/babel.config.js
@@ -3,16 +3,6 @@ module.exports = function (api) {
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            [
-                'module-resolver',
-                {
-                    root: ['./'],
-                    alias: {
-                        '@': './src',
-                    },
-                    extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-                },
-            ],
             'react-native-reanimated/plugin',
         ],
     };
